0501a339c4e6399743170e372d182769e0c754e2
Wayne Graham <wayne.graham@gmail.com>
2024-11-15 13:11:13 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      screens: {
+        print: { raw: 'print'},
+        screen: { raw: 'screen' },
+      }
+    },
+  },
+  plugins: [],
+}
+
