f3e86679a98fa16c045c5ee4f97798db3c9d385c
Wayne Graham <wgraham@clir.org>
2024-11-15 14:01:55 -0500

    Update for brown belt moves

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,14 +6,14 @@ function Navigation() {
     <nav className="bg-white border-gray-200 px- bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <a href="https://waynegraham.github.io/white-to-blue" className="flex">
-          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white underline underline-offset-8">White to Blue</span>
+          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">White to Blue</span>
         </a>
         <a href="https://waynegraham.github.io/blue-to-purple/" className="flex">
           <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Blue to Purple</span>
         </a>
 
         <a href="#" className="flex">
-          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Purple to Brown</span>
+          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white underline underline-offset-8">Purple to Brown</span>
         </a>
 
         <div className="flex md:order-2">
